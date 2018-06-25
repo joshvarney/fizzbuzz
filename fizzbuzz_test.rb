7261a02e5a58dbf89823ef575b0d49f40d342024
Josh Varney <joshcainvarney@gmail.com>
2018-06-25 09:25:49 -0400

    array return test pass

--- fizzbuzz_test.rb
+++ fizzbuzz_test.rb
@@ -8,6 +8,9 @@ class TestFizzbuzz < Minitest::Test
 	def test_if_returns_array
 		assert_equal(Array,fizzbuzz().class)
 	end
+	def test_if_array_has_100_elements
+		assert_equal(100,fizzbuzz().length)
+	end
 
 
 end
