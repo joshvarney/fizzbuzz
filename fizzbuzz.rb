4628dbec7e9bec3999095b0084fa123c6af4261a
Josh Varney <joshcainvarney@gmail.com>
2018-06-25 10:35:51 -0400

    test passed that 30 is mined minds

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,14 @@
 def fizzbuzz()
 	fizz_array = [*1..100]
 	fizz_array.each do |number|
-		if number % 5 == 0
+		if number % 5 == 0 && number % 3 == 0
+			fizz_array[number-1] = "mined minds"
+		elsif number % 5 == 0
 			fizz_array[number-1] = "minds"
 		elsif number % 3 == 0
 			fizz_array[number-1] = "mined"
 		end
 	end 
-	fizz_array[14] = "mined minds"
 
 	fizz_array 
 
