c965e33f8f12b61c58bb99ffb281cb685240dbfa
Josh Varney <joshcainvarney@gmail.com>
2018-06-25 10:27:34 -0400

    passed that 10 = minds

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,13 @@
 def fizzbuzz()
 	x = [*1..100]
 	x.each do |y|
-		if y % 3 == 0
+		if y % 5 == 0
+			x[y-1] = "minds"
+		elsif y % 3 == 0
 			x[y-1] = "mined"
 		end
 	end 
 	x[14] = "mined minds"
-	x[4] = "minds"
 
 	x 
 
